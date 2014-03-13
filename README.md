2dc7ebb4f0b222667f30b8c67dbda17e5b758961
Chetan Surpur <chetan51@gmail.com>
2014-03-13 16:03:07 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Arr.js - Utilities for Array manipulation
+
+## Usage
+
+### Running Tests
+
+    open test/index.html
---
+++ test/arr_test.js
@@ -0,0 +1,11 @@
+describe('Arr.js', function() {
+
+    describe('reshape', function() {
+
+        it('should reshape a 3x2 array into a 2x1 array', function() {
+
+        });
+
+    });
+
+});
